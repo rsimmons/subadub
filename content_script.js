26a6da92f039761f97e8968397f7ed3adb35fc7d
Russel Simmons <russ@rsimmons.org>
2018-12-13 17:39:34 -0800

    Fetch and display vtt subs

--- content_script.js
+++ content_script.js
@@ -7,11 +7,16 @@ el.text = `
   const POLL_INTERVAL_MS = 500;
   const WEBVTT_FMT = 'webvtt-lssdh-ios8';
   const URL_MOVIEID_REGEX = RegExp('/watch/([0-9]+)');
-  const tracksCache = new Map(); // from movie ID to list of available tracks
+  const trackListCache = new Map(); // from movie ID to list of available tracks
+  const webvttCache = new Map(); // from 'movieID/trackID' to blob
   let urlMovieId;
+  let selectedTrackId;
   
-  let targetSubsList;
-  let displayedSubsList;
+  let targetSubsList = null;
+  let displayedSubsList = null;
+
+  let targetTrackBlob = null;
+  let displayedTrackBlob = null;
 
   function extractMovieTextTracks(movieObj) {
     const movieId = movieObj.movieId;
@@ -54,22 +59,61 @@ el.text = `
     }
 
     console.log('CACHING MOVIE TRACKS', movieId, usableTracks);
-    tracksCache.set(movieId, usableTracks);
+    trackListCache.set(movieId, usableTracks);
     renderAndReconcile();
   }
 
+  function fetchWebvtt(movieId, trackInfo) {
+  }
+
   function handleSubsListSetOrChange(selectElem) {
-    console.log('selecting track', selectElem.value);
+    const trackId = selectElem.value;
+    console.log('selecting track', trackId);
+
+    selectedTrackId = trackId;
+
+    if (!selectedTrackId) {
+      return;
+    }
+
+    const movieId = urlMovieId; // this seems a bit hacky to get this way, but should be OK
+    const trackList = trackListCache.get(movieId);
+    const matchingTracks = trackList.filter(el => el.id === trackId);
+    if (matchingTracks.length !== 1) {
+      throw new Error('internal error, no matching track id');
+    }
+    const trackInfo = matchingTracks[0];
+
+    const cacheKey = movieId + '/' + trackInfo.id;
+    if (!webvttCache.has(cacheKey)) {
+      const url = trackInfo.bestUrl;
+
+      fetch(url).then(function(response) {
+        if (response.ok) {
+          return response.blob();
+        }
+        throw new Error('Bad response to WebVTT request');
+      }).then(function(blob) {
+        webvttCache.set(cacheKey, new Blob([blob], {type: 'text/vtt'})); // set type to avoid warning
+        renderAndReconcile();
+      }).catch(function(error) {
+        console.log('Failed to fetch WebVTT file', error.message);
+      });
+    }
+
+    // NOTE: We don't call renderAndReconcile here, caller should do it to avoid recursive loop bug
   }
 
   function renderAndReconcile() {
-    const SUBS_LIST_ID = 'subadub-subs-list'
+    const SUBS_LIST_ELEM_ID = 'subadub-subs-list';
+    const TRACK_ELEM_ID = 'subadub-track';
 
     function addSubsList(tracks) {
       const selectElem = document.createElement('select');
       selectElem.style.cssText = 'position: absolute; z-index: 1000; top: 10px; right: 10px; color: black; font-size: 16px';
       selectElem.addEventListener('change', function(e) {
         handleSubsListSetOrChange(e.target);
+        renderAndReconcile();
       }, false);
 
       let firstCCTrackId;
@@ -88,7 +132,7 @@ el.text = `
       }
 
       const containerElem = document.createElement('div');
-      containerElem.id = SUBS_LIST_ID;
+      containerElem.id = SUBS_LIST_ELEM_ID;
       containerElem.style.cssText = 'width: 100%; height: 100%; position: absolute; top: 0; right: 0; bottom: 0; left: 0';
       containerElem.appendChild(selectElem);
 
@@ -98,27 +142,71 @@ el.text = `
     }
 
     function removeSubsList() {
-      const el = document.getElementById(SUBS_LIST_ID);
+      const el = document.getElementById(SUBS_LIST_ELEM_ID);
+      if (el) {
+        el.remove();
+      }
+    }
+
+    function addTrackElem(videoElem, blob) {
+      const trackElem = document.createElement('track');
+      trackElem.id = TRACK_ELEM_ID;
+      trackElem.src = URL.createObjectURL(blob);
+      trackElem.kind = 'subtitles';
+      trackElem.default = true;
+      // trackElem.mode = 'showing';
+      videoElem.appendChild(trackElem);
+    }
+
+    function removeTrackElem() {
+      const el = document.getElementById(TRACK_ELEM_ID);
       if (el) {
         el.remove();
       }
     }
 
     // Determine what subs list should be
-    if (urlMovieId && (document.readyState === 'complete') && tracksCache.has(urlMovieId)) {
-      targetSubsList = tracksCache.get(urlMovieId);
+    if (urlMovieId && (document.readyState === 'complete') && trackListCache.has(urlMovieId)) {
+      targetSubsList = trackListCache.get(urlMovieId);
     } else {
       targetSubsList = null;
     }
 
     // Reconcile DOM if necessary
-    if (targetSubsList != displayedSubsList) {
-      console.log('updating subs list DOM');
+    if (targetSubsList !== displayedSubsList) {
+      console.log('updating subs list DOM', targetSubsList, displayedSubsList);
+
       removeSubsList();
       if (targetSubsList) {
         addSubsList(targetSubsList);
       }
+
       displayedSubsList = targetSubsList;
+    }
+
+    // Determine what subs blob should be
+    const videoElem = document.querySelector('video');
+    if (urlMovieId && selectedTrackId && videoElem) {
+      const cacheKey = urlMovieId + '/' + selectedTrackId;
+      if (webvttCache.has(cacheKey)) {
+        targetTrackBlob = webvttCache.get(cacheKey);
+      } else {
+        targetTrackBlob = null;
+      }
+    } else {
+      targetTrackBlob = null;
+    }
+
+    // Reconcile DOM if necessary
+    if (targetTrackBlob !== displayedTrackBlob) {
+      console.log('need to update track blob', targetTrackBlob, displayedTrackBlob);
+
+      removeTrackElem();
+      if (targetTrackBlob) {
+        addTrackElem(videoElem, targetTrackBlob);
+      }
+
+      displayedTrackBlob = targetTrackBlob;
     }
   }
 
@@ -149,6 +237,9 @@ el.text = `
       movieId = +movieIdMatch[1];
     }
     urlMovieId = movieId;
+    if (!urlMovieId) {
+      selectedTrackId = null;
+    }
 
     renderAndReconcile();
   }, POLL_INTERVAL_MS);
